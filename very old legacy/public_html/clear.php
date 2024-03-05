ca22fe5c91803819c1fcf1150a71be32b983f1c7
y-martynov <yus.online@gmail.com>
2024-03-05 09:38:50 +0000

    update

--- very old legacy/public_html/clear.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connectDB.php';
-include 'header.php';
-$id = $_GET['id'];
-echo "clear records $id";
-clear($id);
-
-?>
--- very old legacy/public_html/refresh.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connectDB.php';
-include 'header.php';
-$id = $_GET['id'];
-echo "refresh $id";
-refresh($id);
-
-?>
